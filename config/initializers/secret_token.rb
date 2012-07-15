726bcc64637332f9955606b5587d5f7cbc8eecc9
Pawel Szymczykowski <makenai@gmail.com>
2012-07-14 20:46:05 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThirtyDays::Application.config.secret_token = '499d228bffb607e5e465a06c2a3f0b87048f6103a23c96f7d13b35ad6e6bcd1b33ca04eb981a0e78996928bb908cf6802fb3f9b5799925e202a0c81d15994a72'

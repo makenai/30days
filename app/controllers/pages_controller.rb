1b8b8c6c114195abdb82b84b4c8b5ce753ed7943
Pawel Szymczykowski <makenai@gmail.com>
2012-07-18 01:58:17 -0700

    Styled login page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   
   def home
     redirect_to user_root_path if user_signed_in?
+    @inspiration = YAML.load_file( Rails.root.join('config', 'inspiration.yml') ).sort { rand }
   end
   
 end

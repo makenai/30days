72c16c9f33c0f852a15fe2fc96da3ec9131e5a31
Pawel Szymczykowski <makenai@gmail.com>
2012-07-18 02:02:23 -0700

    Not redirecting to home if logged in

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   
   def home
-    redirect_to user_root_path if user_signed_in?
+    # redirect_to user_root_path if user_signed_in?
     @inspiration = YAML.load_file( Rails.root.join('config', 'inspiration.yml') ).sort { rand }
   end
   
